237b46e43cdeedfcf2be72605650afc6fc56bf11
siemarie <jessie.hand@gmail.com>
2014-05-11 17:10:49 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-## Plots histogram of Global Active Power (kw) for the dates of 2/1/2007 - 2/2/2007 ##
+## Plot of Sub metering over time for the dates of 2/1/2007 - 2/2/2007 ##
 
 lines <- grep('^[1-2]/2/2007', readLines('household_power_consumption.txt'))
 skip_num=lines[1]-1
@@ -8,6 +8,9 @@ names(data)<-names(header)
 data[,"Date"]<-as.Date(data[,"Date"],format="%d/%m/%Y")
 data$DateTime<-strptime(paste(data[,"Date"],data[,"Time"],sep=" "), format="%Y-%m-%d %H:%M:%S")
 
-png(filename = "plot2.png",width = 480, height = 480, units = "px", pointsize = 12,bg = "white")
-plot(data$DateTime2,data$Global_active_power,type="l")
+png(filename = "plot3.png",width = 480, height = 480, units = "px", pointsize = 12,bg = "white")
+plot(data$DateTime,data$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="",col="black")
+lines(data$DateTime,data$Sub_metering_2,col="red", type="l")
+lines(data$DateTime,data$Sub_metering_3,col="blue", type="l")
+legend(x="topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),border="black",lty=1)
 dev.off()
